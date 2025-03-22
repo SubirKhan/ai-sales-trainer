0db7a3b7600d2755e0841bc7198a77686516aac7
SubirKhan <subir.khan2020@gmail.com>
2025-03-21 23:01:10 -0700

    Added TypeScript support and updated dependencies

---
+++ .next/static/dKnT_JnO_d_-9eXWLAIEA/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-ed210a42d874d3b5.js"],"/_error":["static/chunks/pages/_error-56a1a2eec59e6cbd.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
